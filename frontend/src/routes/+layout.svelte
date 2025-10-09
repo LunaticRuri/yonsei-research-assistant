2b22a91e9744d7d504b9bad9afd4358164d4e68a
EX3 <inthe_6788@naver.com>
2025-10-09 20:54:30 +0900

    Added tailwind css
    - 테일윈드 css를 붙였습니다.
    
    - TODO:
    1. 외형 꾸미기
    2. 실제로 동작할 수 있게 수정해 나가기

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
+
 	let { children } = $props();
 	let isLoggedIn = false; // 추후 로그인 상태에 따라 변경, 일단은 로그인된 상태로 취급
 	let token: string | null = null;
+
 	if (typeof window !== 'undefined') {
 		token = localStorage.getItem('token');
 		//isLoggedIn = token !== null;
