7416036a8ca3303dbb1cdb2116720d72f7db0800
박민혁 <parker0517@yonsei.ac.kr>
2025-12-04 21:01:41 +0900

    Update prompt

--- backend/strategy_service/core/generator.py
+++ backend/strategy_service/core/generator.py
@@ -101,7 +101,7 @@ class QueryTranslationService:
             input_text = ChatPromptTemplate.from_messages(
                 [
                     ("system", "지금부터 당신은 대학 학술 정보원의 사서입니다. 당신은 정보 이용자가 원하는 자료를 가장 효과적으로 검색할 수 있도록 도와야 합니다."),
-                    ("human", """### 질문: {question}\n            저의 '질문'을 해결하기 위해 제가 검색 엔진에 입력할 '핵심 검색어(Keywords)'들을 쉼표(,)로 구분하여 추출해 주세요. 문장이 아닌 명사형 단어 목록으로만 답변해 주세요. 금지어: '특징', '관련', '선행' ,'연구', '논문', '문헌'""")
+                    ("human", """### 질문: {question}\n            저의 '질문'을 해결하기 위해 제가 검색 엔진에 입력할 '핵심 검색어(Keywords)'들을 쉼표(,)로 구분하여 추출해 주세요. 문장이 아닌 명사형 단어 목록으로만 답변해 주세요. 금지어: '특징', '연구', '논문','문헌'""")
                     ]
             )
             input_text = input_text.format_messages(question=query)
